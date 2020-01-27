f065f65a490351a028a9317b56871004d81cde10
terkmail <terkmail@gmail.com>
2020-01-27 23:56:14 +0200

    updated GUI

--- prototype/GUI/GUI/GUIController.java
+++ prototype/GUI/GUI/GUIController.java
@@ -1,9 +1,24 @@
 package GUI;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+import java.util.LinkedList;
+
+import classes.Catalog;
+import classes.Command;
+import classes.Filter;
+import classes.Item;
+import classes.Order;
+import classes.User;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -11,24 +26,8 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.LinkedList;
-import java.util.ResourceBundle;
-
-import classes.*;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.Node;
 
 
 public class GUIController {
@@ -80,7 +79,10 @@ public class GUIController {
     
     
     /**** Welcome ****/    
-//    @FXML private Button loginBtn;
+    @FXML private Button welcomeLoginBtn;
+    @FXML private Button welcomeOrdersBtn;
+    @FXML private Button welcomeValidateBtn;
+    @FXML private Text generalMsg;
 
     /**** Login ****/ 
     @FXML private TextField loginPassTxt;
@@ -96,6 +98,11 @@ public class GUIController {
 //	@FXML private ResourceBundle resources;
 //  @FXML private URL location;
 
+    /**** Order ****/
+    @FXML private TextArea orderDetailsTA;
+    @FXML private TextField orderAdressTF;
+    @FXML private CheckBox  orderCardCB;
+    @FXML private TextArea orderCardTA;
  
     
     /**** debug ****/
@@ -106,6 +113,13 @@ public class GUIController {
     @FXML void initialize() {
         if(userTxt!=null)
         	userTxt.setText("Hi "+userTxtStr);
+        if(welcomeLoginBtn!=null) {
+        	if(localUser.getPermLevel().equals("SignedUser")) {
+        		welcomeOrdersBtn.setVisible(true);
+        		if( !(localUser.getPermLevel().equals("ValidatedUser")) )
+        			welcomeValidateBtn.setVisible(true);
+        	}
+        }
         if(catalogTable!=null) {
 	        catalogTableName.setCellValueFactory(new PropertyValueFactory<Item, String>("name"));
 	        catalogTablePrice.setCellValueFactory(new PropertyValueFactory<Item, Double>("price"));
@@ -123,6 +137,9 @@ public class GUIController {
         	catalogColorChoice.getItems().add("Red");
         	catalogColorChoice.getItems().add("Blue");
         	catalogColorChoice.getItems().add("Green");
+        }
+        if(orderDetailsTA!=null) {
+        	orderDetailsTA.setText(localOrder.getDetails());
         }
     }
     
@@ -150,20 +167,22 @@ public class GUIController {
     
     @FXML void gotoOrder(ActionEvent event) throws IOException {
 	    if(cart.size()<=0) {
-	    	System.out.println("TODO cart empty msg");
+	    	System.out.println("cart is empty");
+	    	generalMsg.setText("cart is empty");
 	    	return;
 	    }
 	    
-	    if(localUser instanceof User) {
-	    	System.out.println(localUser.getUserName()+" signed");
-		    localOrder.setUser((User)localUser);
-	    }
-	    else {
-	    	System.out.println(localUser.getUserName()+" is a Guest");
-	    	System.out.println("");
+	    if( !(localUser.getPermLevel().equals("SignedUser")) ) {
+	    	System.out.println("permission: "+localUser.getPermLevel()+" expected: SignedUser");
+	    	generalMsg.setText(localUser.getUserName()+", please login first");
+	    	return;
 	    }
 	    
 	    localOrder.setOrdreList(cart);
+	    localOrder.setUser(localUser);
+	    localOrder.setOrderDate(new Date());
+	    
+	    
 	    
     	URL url = getClass().getResource("Order.fxml");
 	    AnchorPane pane = FXMLLoader.load( url );
@@ -172,6 +191,17 @@ public class GUIController {
 	    stage.setTitle("Order");
 	    stage.setScene(scene);
 	}
+    
+    @FXML void orderPurchace(ActionEvent event) throws IOException {
+    	if(localUser.getPermLevel().equals("validatedUser")) {
+    		System.out.println("{---\nsending !order"+localOrder.getDetails()+"---}");
+    		client.ClientConsole.send(new Command("!order",localOrder));
+    	}
+    	else {
+    		gotoSignup(event);
+    	}
+    	
+    }
 
 	@SuppressWarnings("unused")private void _Catalog_() {}
     
@@ -297,7 +327,7 @@ public class GUIController {
 		catalogTxt.setText("add request to item: "+item.stringItem());
 		client.ClientConsole.send(new Command("!addItem",item));
 		int status = replyWait();
-		System.out.println("reply status: "+status);
+		System.out.println("reply recieved: "+(status!=0));
     }
     
     @SuppressWarnings("unused")private void __Login__() {}
@@ -323,10 +353,13 @@ public class GUIController {
 			client.ClientConsole.send(cmd);
 			int status = replyWait();
 			System.out.println("reply recieved: "+(status!=0));
-			if(reply.msg.equals("Log In Success"))
-				localUser=(User)cmd.obj;
+			if(reply.msg.equals("Log In Success")) {
+				localUser=(User)reply.obj;
+				gotoWelcome(event);
+			}
+			else
+				generalMsg.setText(reply.msg);
 		}
-		gotoWelcome(event);
 	}
 
 	@FXML void gotoLogin(ActionEvent event) throws IOException{
@@ -355,13 +388,39 @@ public class GUIController {
 		cmd.obj=new User(loginUserTxt.getText(),loginPassTxt.getText());
 		client.ClientConsole.send(cmd);
 		int status = replyWait();
-		System.out.println("reply status: "+status);
-		gotoSignup(event);
+		System.out.println("reply recieved: "+(status!=0));
+		if(reply.msg.contentEquals("Success")) {
+			
+			cmd.obj=new User(loginUserTxt.getText(),loginPassTxt.getText());
+			client.ClientConsole.send(new Command("!login",reply.obj));
+			status = replyWait();
+			System.out.println("reply recieved: "+(status!=0));
+			if(reply.msg.equals("Log In Success")) {
+				localUser=(User)reply.obj;
+				gotoWelcome(event);
+			}
+			else
+				generalMsg.setText(reply.msg);
+			gotoWelcome(event);
+		}
+		else {
+//			System.out.println("signUp failed");
+			generalMsg.setText("signUp failed, "+reply.msg);
+		}
 	}
 
-	@FXML void signupVarify(ActionEvent event){
-		System.out.println("TODO: signupVarify implement");
-//		signupTxt.setText("varify unavailable");
+	@FXML void signupValidate(ActionEvent event) throws InterruptedException, IOException{
+		client.ClientConsole.send(new Command("!validate"));
+		int status = replyWait();
+		System.out.println("reply recieved: "+(status!=0));
+		if(reply.msg.equals("User Valid")) {
+			System.out.println("User Validated");
+			gotoCatalog(event);
+		}
+		else {
+			System.out.println("validation failed");
+		}
+		
 	}
 	
 	@SuppressWarnings("unused")private void __Welcome__() {}
@@ -387,7 +446,7 @@ public class GUIController {
     @FXML void debugSend(ActionEvent event) throws InterruptedException {
 		client.ClientConsole.send(new Command(debugCommandTxt.getText()));
 		int status = replyWait();
-		System.out.println("reply status: "+status);
+		System.out.println("reply recieved: "+(status!=0));
 		debugObjectTxt.setText(clientMsg);
     }
 
